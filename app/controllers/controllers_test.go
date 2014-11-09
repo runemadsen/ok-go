bc28e825a47a9b0df82686279fe5ebd4a8199039
Rune Madsen <rune@runemadsen.com>
2014-11-08 23:17:02 -0500

    more

--- app/controllers/controllers_test.go
+++ app/controllers/controllers_test.go
@@ -1,13 +1,31 @@
-package controllers_test
+package test_test
 
 import (
+  "github.com/go-martini/martini"
+  config "golang-rails-template/config"
   . "github.com/onsi/ginkgo"
   . "github.com/onsi/gomega"
-
+  "net/http"
+  "net/http/httptest"
   "testing"
 )
 
-func TestGolangRailsTemplate(t *testing.T) {
+var (
+  m *martini.ClassicMartini
+  response *httptest.ResponseRecorder
+)
+
+func TestTest(t *testing.T) {
   RegisterFailHandler(Fail)
-  RunSpecs(t, "GolangRailsTemplate Suite")
+  RunSpecs(t, "Test Suite")
+}
+
+var _ = BeforeSuite(func() {
+  m = config.CreateApplication()
+})
+
+func Request(method string, route string) {
+  request, _ := http.NewRequest(method, route, nil)
+  response = httptest.NewRecorder()
+  m.ServeHTTP(response, request)
 }
